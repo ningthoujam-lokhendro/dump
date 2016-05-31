ec421fce1ea4ef1d2764d2a833985d9d7dc0c23e
Lokhendro <Lokhendro@gmail.com>
2016-05-31 12:51:27 +0530

    extra package

--- vagrant/install-extra.sh
+++ vagrant/install-extra.sh
@@ -12,3 +12,8 @@ sudo apt-get install --force-yes -y apache2
 service apache2 restart
 sudo apt-get install --force-yes -y curl
 sudo apt-get install --force-yes -y vim
+sudo apt-get install --force-yes -y input-utils
+sudo apt-get install --force-yes -y android-tools-adb android-tools-fastboot
+curl -sL https://deb.nodesource.com/setup | sudo bash -
+sudo apt-get install --force-yes -y nodejs
+sudo apt-get install --force-yes -y build-essential
